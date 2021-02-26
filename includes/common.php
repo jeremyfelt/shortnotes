6b8c005668ebbfdc3b1bb09ed4442b11ae4cc02f
jeremyfelt <jeremy.felt@gmail.com>
2021-02-25 18:19:00 -0800

    Add initial file structure for shortnotes

---
+++ includes/common.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ShortNotes\Common;
+
+// Alpha.
+// [ ] Register a custom post type for notes.
+// [ ] Apply a very basic block template to the post type.
+// [ ] Add webmention support to post type
+// [ ] Post notes to Twitter.
+// [ ] Inject existing tweets as notes
+
+/**
+ * Retrieve the plugin version.
+ *
+ * @return string The plugin version.
+ */
+function get_version() {
+	return '0.0.1';
+}
---
+++ plugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name:     Short Notes
+ * Plugin URI:      https://github.com/jeremyfelt/shortnotes/
+ * Description:     Add a notes post type to WordPress. For your short notes.
+ * Author:          jeremyfelt
+ * Author URI:      https://jeremyfelt.com
+ * Text Domain:     shortnotes
+ * Domain Path:     /languages
+ * Version:         0.0.1
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// This plugin, like WordPress, requires PHP 5.6 and higher.
+if ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
+	add_action( 'admin_notices', 'shortnotes_admin_notice' );
+	/**
+	 * Display an admin notice if PHP is not 5.6.
+	 */
+	function shortnotes_admin_notice() {
+		echo '<div class=\"error\"><p>';
+		echo __( 'The Short Notes WordPress plugin requires PHP 5.6 to function properly. Please upgrade PHP or deactivate the plugin.', 'shortnotes' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo '</p></div>';
+	}
+
+	return;
+}
+
+require_once __DIR__ . '/includes/common.php';
