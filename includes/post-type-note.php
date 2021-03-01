1bf535aafa2040335cf43bfe22a9740a29bcc621
jeremyfelt <jeremy.felt@gmail.com>
2021-02-28 17:19:06 -0800

    Introduce title/slug handling for notes
    Notes aren't really meant to have "titles", but it's still nice to
    have something to use as the document title for search engines and
    other lists of notes.

--- includes/post-type-note.php
+++ includes/post-type-note.php
@@ -4,6 +4,7 @@ namespace ShortNotes\PostType\Note;
 
 add_action( 'init', __NAMESPACE__ . '\register_post_type', 10 );
 add_filter( 'allowed_block_types', __NAMESPACE__ . '\filter_allowed_block_types', 10, 2 );
+add_filter( 'wp_insert_post_data', __NAMESPACE__ . '\filter_wp_insert_post_data', 10 );
 
 /**
  * Provide the common slug used for the Notes post type.
@@ -62,4 +63,81 @@ function filter_allowed_block_types( $allowed_block_types, $post ) {
 	}
 
 	return $allowed_block_types;
+}
+
+/**
+ * Provide a default, placeholder title used when a note is first created
+ * as an alternative to "Auto Draft".
+ *
+ * @return string The placeholder title.
+ */
+function get_placeholder_title() {
+	return __( 'Note', 'shortnotes' );
+}
+
+/**
+ * Format the note's title to be slightly more descriptive and provide a
+ * bit more information about the note.
+ *
+ * @param array A list of data about the note.
+ * @return string The formatted title.
+ */
+function get_formatted_title( $post_data ) {
+	$blocks = parse_blocks( $post_data['post_content'] );
+
+	// Retrieve the site's preferred date and time formats.
+	$date_format = get_option( 'date_format', 'F n, Y' );
+	$time_format = get_option( 'time_format', 'g:ia' );
+
+	// Retrieve a localized and formatted version of the note's create date. I don't think
+	// it's translated in the best way yet, but I'll figure that out soon?
+	$sub_title = wp_date( $date_format . ' \a\t ' . $time_format, strtotime( $post_data['post_date_gmt'] ) );
+
+	foreach ( $blocks as $block ) {
+		if ( 'core/paragraph' === $block['blockName'] ) {
+			$sub_title = wp_strip_all_tags( $block['innerHTML'] );
+
+			// At the risk of being complicated, determine the length of the translated "Note" pretext so
+			// that we can build a maximum string of 50 characters.
+			$string_lenth = 50 - strlen( get_placeholder_title() );
+
+			// If the note text is less then the max string length, use the full text. If not, append an ellipsis.
+			$sub_title = $string_lenth >= mb_strlen( $sub_title ) ? $sub_title : substr( $sub_title, 0, $string_lenth ) . '&hellip;';
+
+			// A paragraph has been found, we're moving on and using it for the title.
+			continue;
+		} elseif ( 'core/image' === $block['blockName'] ) {
+			$sub_title = __( 'Image posted on', 'shortnotes' ) . ' ' . $sub_title;
+		} elseif ( 'core/gallery' === $block['blockName'] ) {
+			$sub_title = __( 'Images posted on', 'shortnotes' ) . ' ' . $sub_title;
+		}
+	}
+
+	return 'Note: ' . $sub_title;
+}
+
+/**
+ * Filter post data when it is inserted to ensure a proper slog and title
+ * has been generated.
+ *
+ * Slugs (post_name) are the first 4 characters of a UUID4 combined with
+ * a unix timestamp. It's like creative, but not... :)
+ *
+ * Titles are a placeholder until published and then they are generated
+ * with `get_formatted_title()` based on the content.
+ *
+ * @param array $post_data A list of data about the post to be updated.
+ * @return array $post_data A modified list of post data.
+ */
+function filter_wp_insert_post_data( $post_data ) {
+	if ( get_slug() === $post_data['post_type'] && 'Auto Draft' === $post_data['post_title'] ) {
+		$post_data['post_title'] = get_placeholder_title();
+		$post_data['post_name']  = substr( wp_generate_uuid4(), 0, 4 ) . time();
+	}
+
+	if ( 'publish' === $post_data['post_status'] ) {
+		$post_data['post_title'] = get_formatted_title( $post_data );
+	}
+
+	return $post_data;
 }
