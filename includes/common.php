32794ede9e123a6000784c21d417c3f1566c4ca3
jeremyfelt <jeremy.felt@gmail.com>
2021-02-28 18:05:26 -0800

    Remove alpha idea notes

--- includes/common.php
+++ includes/common.php
@@ -2,17 +2,6 @@
 
 namespace ShortNotes\Common;
 
-// Alpha.
-// [x] Register a custom post type for notes.
-// [x] Apply a very basic block template to the post type.
-// [x] Auto-generate slugs that aren't auto-draft
-// [x] Auto-generate date-sepcific titles
-// [x] Update the title when the note's publish date is changed.
-// [x] Make sure feeds work as expected
-// [ ] Add webmention support to post type.
-// [ ] Post notes to Twitter. (shortnotes-to-twitter plugin)
-// [ ] Inject existing tweets as notes (shortnotes-from-twitter plugin)
-
 /**
  * Retrieve the plugin version.
  *
