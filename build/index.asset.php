fbcfbf2673c4331dde00a4d5cd98e5d24752a070
jeremyfelt <jeremy.felt@gmail.com>
2021-07-24 11:20:53 -0700

    Update @wordpress/scripts to 17.0.0, rebuild assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => '64fbe44a3ed902e70c1c7efeec786b38');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => 'ca9d0eea85632f65941550a0e0dd3370');
