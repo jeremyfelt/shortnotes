b4d8c72361bcc5c7747c3c2f090f49d6b2dbe696
Jeremy Felt <jeremy.felt@gmail.com>
2022-03-22 16:53:44 -0700

    Auto-fix JS linting issues

--- src/index.js
+++ src/index.js
@@ -5,53 +5,50 @@ import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { useDispatch, useSelect } from '@wordpress/data';
 
 const ReplyToSideBarPanel = () => {
-	const meta = useSelect( ( select ) =>
-		select( 'core/editor' ).getEditedPostAttribute( 'meta' )
+	const meta = useSelect((select) =>
+		select('core/editor').getEditedPostAttribute('meta')
 	);
 
-	const { editPost } = useDispatch( 'core/editor' );
+	const { editPost } = useDispatch('core/editor');
 
-	const setMetaValue = ( key, value ) => {
-		editPost( {
+	const setMetaValue = (key, value) => {
+		editPost({
 			meta: {
-				[ key ]: value,
+				[key]: value,
 			},
-		} );
+		});
 	};
 
 	return (
 		<PluginDocumentSettingPanel
 			name="reply-to-panel"
-			title={ __( 'Reply to', 'shortnotes' ) }
-			icon={ false }
+			title={__('Reply to', 'shortnotes')}
+			icon={false}
 		>
 			<TextareaControl
-				label={ __( 'Reply to URL (optional)', 'shortnotes' ) }
-				help={ __(
-					'Enter a URL if this note is a reply',
-					'shortnotes'
-				) }
-				value={ meta.shortnotes_reply_to_url }
-				onChange={ ( value ) =>
-					setMetaValue( 'shortnotes_reply_to_url', value )
+				label={__('Reply to URL (optional)', 'shortnotes')}
+				help={__('Enter a URL if this note is a reply', 'shortnotes')}
+				value={meta.shortnotes_reply_to_url}
+				onChange={(value) =>
+					setMetaValue('shortnotes_reply_to_url', value)
 				}
 			/>
 			<TextareaControl
-				label={ __( 'Reply to name (optional)', 'shortnotes' ) }
-				help={ __(
+				label={__('Reply to name (optional)', 'shortnotes')}
+				help={__(
 					'Enter a name this reply is directed to',
 					'shortnotes'
-				) }
-				value={ meta.shortnotes_reply_to_name }
-				onChange={ ( value ) =>
-					setMetaValue( 'shortnotes_reply_to_name', value )
+				)}
+				value={meta.shortnotes_reply_to_name}
+				onChange={(value) =>
+					setMetaValue('shortnotes_reply_to_name', value)
 				}
 			/>
 		</PluginDocumentSettingPanel>
 	);
 };
 
-registerPlugin( 'reply-to-panel', {
+registerPlugin('reply-to-panel', {
 	render: ReplyToSideBarPanel,
 	icon: '',
-} );
+});
