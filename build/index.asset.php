927af485938657ea9ed7745953ed311d47c0a065
Jeremy Felt <jeremy.felt@gmail.com>
2022-06-13 17:25:24 -0700

    Update NPM dependencies

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '4bbe57c69d9fb8277cb9acb1cbe15380');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'ccbee4ae86a23daf7f60');
