4a79150635708674fe1936113dc183d86dac11d3
Jeremy Felt <jeremy.felt@gmail.com>
2023-07-15 15:29:42 -0700

    Update `@wordpress/scripts` 26.8.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'ccbee4ae86a23daf7f60');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '508f05b7a61d9b3980de');
