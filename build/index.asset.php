8289351deb3afc5d6bba7f3bd74627b57a677265
jeremyfelt <jeremy.felt@gmail.com>
2025-07-04 06:37:21 -0700

    Import `PluginDocumentSettingPanel` from `@wordpress/editor`
    This was moved in WordPress 6.6.
    
    See https://make.wordpress.org/core/2024/06/18/editor-unified-extensibility-apis-in-6-6/

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '4e2e49cbb718c8260fd8');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => 'fb9d335ac74f4fa2c1d4');
